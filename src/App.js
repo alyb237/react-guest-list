0c35191da5a4876abd5e3cd4229eb39dfb11c022
alyb237 <alyb237@gmail.com>
2022-05-23 16:35:59 +0200

    Fix drone error with remove button

--- src/App.js
+++ src/App.js
@@ -165,7 +165,7 @@ function App() {
                     aria-label={`Remove ${guests.firstName} ${guests.lastName}`}
                     onClick={() => deleteGuest(guests.id)}
                   >
-                    Delete
+                    Remove
                   </button>
                 </li>
               </div>
